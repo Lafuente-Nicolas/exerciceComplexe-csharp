d8cf31a0d86ca076a2e0ca6517ed14c85ba23e96
Nicolas Lafuente <lafuentenicolas1@gmail.com>
2025-06-26 10:42:33 +0200

    Ajoutez des fichiers projet.

---
+++ NotesUtil.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exerciceComplexe
+{
+    internal class NotesUtil
+    {
+
+        public static void CalculerMoyenne(int totalPoints, int nombreNotes)
+        {
+            double moyenne = totalPoints / nombreNotes;
+            Console.WriteLine($"La moyenne est : {moyenne}");
+
+        }
+        // Surcharge de la fonction
+
+        public static double CalculerMoyenne(List<int> notes)
+        {
+            double sommes = 0;
+            double moyenne = 0;
+
+            foreach (int n in notes)
+                sommes += n;
+
+            moyenne = sommes / notes.Count;
+
+            return moyenne;
+        }
+
+
+     
+        public void AfficherStats(List<int> notesList)
+        {
+            double moyenneList = CalculerMoyenne(notesList);
+            Console.WriteLine($"La valeur maximale est {notesList.Max()}");
+            Console.WriteLine($"La valeur minimale est {notesList.Min()}");
+            Console.WriteLine($"La moyenne est {moyenneList}");
+        }
+
+
+    }
+};
+
+/*Console.WriteLine("le total des points obtenus : ");
+totalPoints = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("le nombre de notes : ");
+nombreNotes = Convert.ToInt32(Console.ReadLine());*/
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System.ComponentModel.Design;
+
+namespace exerciceComplexe
+    
+{
+
+    internal class Program
+
+    {
+        static void Main(string[] args)
+        {
+            NotesUtil.CalculerMoyenne(3, 2);
+            int nombreNotes = 0;
+            Console.Write("le nombre de notes : ");
+            nombreNotes = Convert.ToInt32(Console.ReadLine());
+            List<int> stock = new List<int>();
+            Console.WriteLine("Saisissez les notes: ");
+            for (int count = 0; count < nombreNotes; count++)
+            {
+                stock.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+            NotesUtil.CalculerMoyenne(stock);
+            Console.WriteLine($"la moyenne est de {NotesUtil.CalculerMoyenne(stock)}");
+
+            NotesUtil stats = new NotesUtil();
+            stats.AfficherStats(stock); ;
+        }
+        
+
+    }
+};
+
